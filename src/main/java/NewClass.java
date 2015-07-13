46a7b3dabd9b00f6eeba02a4435599d4dda75415
mratasoy <yunusonuratasoy@gmail.com>
2015-07-13 11:42:44 +0300

    bitti

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -10,4 +10,15 @@
  */
 public class NewClass {
     
+    private String str ;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+    
+    
 }
